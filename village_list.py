db3e1b5d5d42e3a209fa2faf707789f5c9859ee1
Bazigaba <olivier.bazigaba@adfinance.co>
2018-11-26 14:29:37 +0200

    Update village_list.py
    Add first if condition in code

--- village_list.py
+++ village_list.py
@@ -1,3 +1,4 @@
 villages={v_no1:1,v_name="Amariza",v_no1:1,v_name="Amariza",v_no2:2,v_name2="Inyange",v_no3:3,v_name3="Inyamanza"}
-for vilage in villages
-print(village)
+for village in villages
+if(village.v_no1==1)
+print(v_name.village)
